93db19bf5a95af7a06f3dab8e591c5b99545ad46
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-24 16:53:53 -0300

    Hello World - Click on a button and change an object's color

---
+++ Assets/Scenes/hello world/ClickOnButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickOnButton : MonoBehaviour
+{
+    public Button button;
+    public GameObject targetBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        Debug.Log("Clicou");
+        var targetBoxRenderer = targetBox.GetComponent<MeshRenderer>();
+        Color newColor = new Color(Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        targetBoxRenderer.material.color = newColor;
+    }
+}
