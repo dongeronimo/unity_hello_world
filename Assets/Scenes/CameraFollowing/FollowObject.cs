8252635a382a47eb38287b7ee63dd2d9a406af5d
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-24 19:59:07 -0300

    Movement and collisions

---
+++ Assets/Scenes/CameraFollowing/FollowObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject Camera;
+    public Vector3 CameraPositionRelativeToTarget;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraPosition = Target.transform.position + CameraPositionRelativeToTarget;
+        Camera.transform.position = cameraPosition;
+        Camera.transform.LookAt(Target.transform);    
+    }
+}
---
+++ Assets/Scenes/CameraFollowing/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float movementCoeficent = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var verticalMovement = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        var horizontalMovement = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        player.transform.position = player.transform.position +
+            new Vector3(horizontalMovement, 0, verticalMovement);
+    }
+   
+
+}
