d2b27ae9b2666366b126952b343fd378234e7fd3
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-25 00:42:04 -0300

    Fiz a tela de welcome e consegui gerar a 1a build

---
+++ Assets/Scenes/CameraFollowing/BackToWelcomeClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class BackToWelcomeClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     //   
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("Scenes/Welcome/Welcome");
+    }
+}
---
+++ Assets/Scenes/Welcome/MoveWithKeyboardAndCollisionClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class MoveWithKeyboardAndCollisionClick : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Scenes/CameraFollowing/CameraFollowing");
+    }
+}
