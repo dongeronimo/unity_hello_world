63ccb304b8e9d28e36915a740f8fd9a8b1fc8274
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-25 19:52:08 -0300

    Aprendi como criar uma nova janela no editor da unity

---
+++ Assets/EditorExtensions/ExampleWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+/// <summary>
+/// Baseado em https://www.youtube.com/watch?v=491TSNwXTIg
+/// </summary>
+public class ExampleWindow : EditorWindow
+{
+    [MenuItem("Geronimo/Example")]
+    public static void ShowWindow()
+    {
+        GetWindow<ExampleWindow>("Example");
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Hello World");
+        if (GUILayout.Button("Press me"))
+        {
+            Debug.Log("pressed");
+        }
+    }
+}
