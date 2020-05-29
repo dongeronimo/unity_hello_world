d3efdb0a25c6ceaaf08c2cf566ba9177c62f48de
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-29 19:10:00 -0300

    Saindo da Welcome pra Character Animation Part 1

---
+++ Assets/Scenes/Welcome/CharacterAnimationPart1Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharacterAnimationPart1Button : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("Scenes/Character Animations/Character Animation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
