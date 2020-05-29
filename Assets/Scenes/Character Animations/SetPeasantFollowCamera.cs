de87e23709a24a50c2a269a8c959ef72bca79a93
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-29 20:02:59 -0300

    O PT_Medieval_Male está dentro de um GameObject Peasant. Peasant tem um GameObject PeasantCameraPOV, Peasant tem um script de posicionamento da camera pra acompanhar o Peasant. O script usa a Main Camera

---
+++ Assets/Scenes/Character Animations/SetPeasantFollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPeasantFollowCamera : MonoBehaviour
+{
+    public Camera CharacterCamera;
+    public Transform POV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterCamera.transform.position = POV.position;
+        CharacterCamera.transform.LookAt(this.transform.position);
+    }
+}
