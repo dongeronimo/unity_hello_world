c9615913d44821361879cf01ec94cdd3c4241507
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-29 21:01:18 -0300

    Criei o parametro Forward no Animator. Criei as transições Idle-Walk de acordo com o valor de forward. O valor de forward é setado no update de acordo com o valor do eixo vertical do Input.

---
+++ Assets/Scenes/Character Animations/PeasantActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeasantActions : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); //* Time.deltaTime;
+        float v = Input.GetAxisRaw("Vertical"); //* Time.deltaTime;
+        Debug.Log(v);
+        animator.SetFloat("Forward", v);
+
+    }
+}
