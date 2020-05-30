87e4d5cc7619e8e706eea73b83e9ec5a1b60c764
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-29 21:44:58 -0300

    Com o animator forward mudando a posição do Peasant eu consigo andar para frente

--- Assets/Scenes/Character Animations/AnimatorForward.cs
+++ Assets/Scenes/Character Animations/AnimatorForward.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class AnimatorForward : MonoBehaviour
 {
-
     void OnAnimatorMove()
     {
        
         Animator animator = GetComponent<Animator>();
         if (animator)
         {
+            Vector3 newPosition = transform.parent.position;
+            newPosition.z += animator.GetFloat("Forward") * Time.deltaTime;
+            transform.parent.position = newPosition;
+            /*
             Vector3 newPosition = transform.position;
             newPosition.z += animator.GetFloat("Forward") * Time.deltaTime;
             transform.position = newPosition;
+            */
         }
        
     } 
