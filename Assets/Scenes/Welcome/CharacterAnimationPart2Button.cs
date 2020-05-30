eecd3d21d4142bd8fff3bc2c364282d8d239ad08
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-30 15:21:29 -0300

    Caminho para a animation reuse na tela de welcome. Retorno para welcome na tela de animation reuse

---
+++ Assets/Scenes/Welcome/CharacterAnimationPart2Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class CharacterAnimationPart2Button : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Scenes/Animation Reuse/Character Animation Reuse");
+    }
+}
