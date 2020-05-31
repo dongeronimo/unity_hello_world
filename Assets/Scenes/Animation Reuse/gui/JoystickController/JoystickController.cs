681b8a4d72c9251fe9ef4a002b4756a768ff3009
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-30 23:14:24 -0300

    Está rotacionando o boneco quando eu uso o controle estilo freefire

--- Assets/Scenes/Animation Reuse/gui/JoystickController/JoystickController.cs
+++ Assets/Scenes/Animation Reuse/gui/JoystickController/JoystickController.cs
@@ -3,68 +3,149 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+//TODO: Mandar pro boneco
+//TODO: Ocultar se não estiver em browser mobile
 public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
     IDragHandler, IEndDragHandler
 {
     public Image JoystickTouchImage;
-
-    public void OnDrag(PointerEventData eventData)
+    public JoystickOrientationReceiver OrientationReceiver;
+    private Vector2 lastEventPositionInScreenCoordinates = new Vector2();
+    private bool isPressed = false;
+    private void ApplyToReceiver(Vector2 movementAxes)
     {
-        ModifyJoystickTouchImage(eventData.position);
-        Vector2 movementAxes = GetMovementAxes(eventData);
-        //TODO: mandar pro boneco
+        OrientationReceiver.SetOrientation(movementAxes);
     }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        ModifyJoystickTouchImage(eventData.position);
-        Vector2 movementAxes = GetMovementAxes(eventData);
-        //TODO: mandar pro boneco
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        JoystickTouchImage.enabled = true;
-        ModifyJoystickTouchImage(eventData.position);
-        Vector2 movementAxes = GetMovementAxes(eventData);
-        //TODO: mandar pro boneco
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        JoystickTouchImage.enabled = false;
-        ModifyJoystickTouchImage(eventData.position);
-        Vector2 movementAxes = GetMovementAxes(eventData);
-        //TODO: mandar pro boneco
-    }
-    private void ModifyJoystickTouchImage(Vector2 newPosition)
-    {
-        JoystickTouchImage.GetComponent<RectTransform>().position = newPosition;
-    }
-    private Vector2 getEventPositionInLocalCoordinates(PointerEventData eventData)
+    private Vector2 GetEventPositionInLocalCoordinates(Vector2 position)
     {
         Vector2 joystickScreenCoordinates = GetComponent<RectTransform>().position;
-        Vector2 eventScreenCoordinates = eventData.position;
+        Vector2 eventScreenCoordinates = position;
         Vector2 eventLocalCoordinates = joystickScreenCoordinates - eventScreenCoordinates;
         return eventLocalCoordinates;
     }
-    private Vector2 GetMovementAxes(PointerEventData eventData)
+    private Vector2 GetMovementAxes(Vector2 eventDataPosition)
     {
         var area = GetComponent<RectTransform>().rect;
-        Vector2 positionInLocalCoord = getEventPositionInLocalCoordinates(eventData);
+        Vector2 positionInLocalCoord = GetEventPositionInLocalCoordinates(eventDataPosition);
         var horizontalMovement = -positionInLocalCoord.x / area.width * 2.0f;
         var verticalMovement = -positionInLocalCoord.y / area.height * 2.0f;
         var movementAxesVector = new Vector2(horizontalMovement, verticalMovement);
         movementAxesVector.Normalize();
         return movementAxesVector;
     }
-    void Start()
+    private void ModifyJoystickTouchImage(Vector2 newPosition)
     {
-        JoystickTouchImage.enabled = false;    
+        JoystickTouchImage.GetComponent<RectTransform>().position = newPosition;
     }
-    
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        lastEventPositionInScreenCoordinates = eventData.position;
+        isPressed = true;
+        //throw new System.NotImplementedException();
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lastEventPositionInScreenCoordinates = eventData.position;
+        isPressed = false;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        lastEventPositionInScreenCoordinates = eventData.position;
+        isPressed = true;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        lastEventPositionInScreenCoordinates = eventData.position;
+        isPressed = false;
+        //throw new System.NotImplementedException();
+    }
+    public void Start()
+    {
+        JoystickTouchImage.enabled = false;
+    }
+    public void Update()
+    {
+        if(isPressed)
+        {
+            JoystickTouchImage.enabled = true;
+            ModifyJoystickTouchImage(lastEventPositionInScreenCoordinates);
+            Vector2 movementAxes = GetMovementAxes(lastEventPositionInScreenCoordinates);
+            ApplyToReceiver(movementAxes);
+        }
+        else
+        {
+            JoystickTouchImage.enabled = false;
+            ApplyToReceiver(new Vector2());
+        }
+    }
+    /*
+public void OnDrag(PointerEventData eventData)
+{
+  Debug.Log("OnDrag");
+  ModifyJoystickTouchImage(eventData.position);
+  Vector2 movementAxes = GetMovementAxes(eventData);
+  ApplyToReceiver(movementAxes);
+}
+
+private void ApplyToReceiver(Vector2 movementAxes)
+{
+  OrientationReceiver.SetOrientation(movementAxes);
+}
+
+public void OnEndDrag(PointerEventData eventData)
+{
+  Debug.Log("OnDrag");
+  ModifyJoystickTouchImage(eventData.position);
+  Vector2 movementAxes = GetMovementAxes(eventData);
+  ApplyToReceiver(movementAxes);
+}
+
+public void OnPointerDown(PointerEventData eventData)
+{
+  Debug.Log("OnDrag");
+  JoystickTouchImage.enabled = true;
+  ModifyJoystickTouchImage(eventData.position);
+  Vector2 movementAxes = GetMovementAxes(eventData);
+  ApplyToReceiver(movementAxes);
+}
+
+public void OnPointerUp(PointerEventData eventData)
+{
+  Debug.Log("OnDrag");
+  JoystickTouchImage.enabled = false;
+  ModifyJoystickTouchImage(eventData.position);
+  Vector2 movementAxes = GetMovementAxes(eventData);
+  ApplyToReceiver(movementAxes);
+}
+private void ModifyJoystickTouchImage(Vector2 newPosition)
+{
+  JoystickTouchImage.GetComponent<RectTransform>().position = newPosition;
+}
+private Vector2 GetEventPositionInLocalCoordinates(PointerEventData eventData)
+{
+  Vector2 joystickScreenCoordinates = GetComponent<RectTransform>().position;
+  Vector2 eventScreenCoordinates = eventData.position;
+  Vector2 eventLocalCoordinates = joystickScreenCoordinates - eventScreenCoordinates;
+  return eventLocalCoordinates;
+}
+private Vector2 GetMovementAxes(PointerEventData eventData)
+{
+  var area = GetComponent<RectTransform>().rect;
+  Vector2 positionInLocalCoord = GetEventPositionInLocalCoordinates(eventData);
+  var horizontalMovement = -positionInLocalCoord.x / area.width * 2.0f;
+  var verticalMovement = -positionInLocalCoord.y / area.height * 2.0f;
+  var movementAxesVector = new Vector2(horizontalMovement, verticalMovement);
+  movementAxesVector.Normalize();
+  return movementAxesVector;
+}
+void Start()
+{
+  JoystickTouchImage.enabled = false;    
+}
+*/
 }
---
+++ Assets/Scenes/Animation Reuse/gui/JoystickController/JoystickOrientationReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickOrientationReceiver : MonoBehaviour
+{
+    public void SetOrientation(Vector2 normalizedOrientation)
+    {
+        //Debug.Log("SetOrientation " + normalizedOrientation);
+        //TODO: Aplicar o movimento forward/backward.
+        var animator = GetComponent<Animator>();
+        animator.SetFloat("Forward", normalizedOrientation.y);
+        //TODO: Aplicar o movimento de rotaçào
+        transform.parent.transform.Rotate(new Vector3(0, 1, 0), 90 *normalizedOrientation.x* Time.deltaTime);
+    }
+}
